7f6e835b2b31465317b4d60100735ec01a450998
5aurav <saurav1467.be23@chitkarauniversity.edu.in>
2026-09-14 10:38:48 +0530

    UC1: create Contact class with ToString

--- AddressBookApp/Program.cs
+++ AddressBookApp/Program.cs
@@ -1,1 +1,24 @@
-﻿Console.WriteLine("Hello, World!");
+﻿using AddressBookApp.Models;
+
+namespace AddressBookApp
+{
+    class Program
+    {
+        static void Main(String[] args)
+        {
+            Contact contact = new Contact(
+            "Saurav",
+            "Yadav",
+            "House no 585 Sector 32A",
+            "Chandigarh",
+            "Chandigarh",
+            "160030",
+            "7087626063",
+            "sauravkumaryadav2442005@gmail.com"
+             );
+
+            Console.WriteLine(contact.ToString());
+
+        }
+    }
+}
